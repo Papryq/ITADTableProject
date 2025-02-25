68fb71657c763c9ba1982733fa3180a6bc546ddf
Papryq <piq1998pll@wp.pl>
2025-02-25 23:15:03 +0100

    fix

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+
+app.use(cors({ origin: "https://itadtableproject.onrender.com/api/auth", credentials: true }))
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
