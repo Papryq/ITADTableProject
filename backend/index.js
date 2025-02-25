48e5267fc196632783d0554c376d455c7b984270
Papryq <piq1998pll@wp.pl>
2025-02-25 23:27:25 +0100

    Cors update

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors({ origin: "https://itadtableproject.onrender.com/api/auth", credentials: true }))
+app.use(cors({ 
+  origin: "https://itad-table-project-f4hmuk13d-patryks-projects-e3db33f8.vercel.app",  
+  credentials: true 
+}));
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
